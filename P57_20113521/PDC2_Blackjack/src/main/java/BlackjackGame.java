e52af9004fcf7fb3263f79267b9541418142fb48
Tobyko48 <tobyko48@gmail.com>
2023-10-27 20:28:34 +1300

    Update BlackjackGame.java
    Creates profiles table for database.
    Calls Login UI to start game

--- P57_20113521/PDC2_Blackjack/src/main/java/BlackjackGame.java
+++ P57_20113521/PDC2_Blackjack/src/main/java/BlackjackGame.java
@@ -8,15 +8,17 @@
  * @author Pikachu
  */
 
+import javax.swing.SwingUtilities;
 
 public class BlackjackGame {
 
     public static void main(String[] args) {
-        // Create a new game
-        BlackjackGUI game = new BlackjackGUI();
-
-        // Play the game
-        game.playGame();
-             
+        // Create Profiles database table first
+        ProfileDatabase.createTable();
+        
+        // Start Game by invoking LoginUI
+        SwingUtilities.invokeLater(() -> {
+            new LoginUI();
+        });
     }
 }
