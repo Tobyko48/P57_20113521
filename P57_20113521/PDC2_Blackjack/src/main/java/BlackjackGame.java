8da23ef9cf7806cd4e5e76d3fbdc078aff2496d9
Tobyko48 <tobyko48@gmail.com>
2023-10-27 17:37:34 +1300

    New classes
    Increasing modularity and encapsulation.
    Creating class for database.
    Refactored classes for better clarity

---
+++ P57_20113521/PDC2_Blackjack/src/main/java/BlackjackGame.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Pikachu
+ */
+
+
+public class BlackjackGame {
+
+    public static void main(String[] args) {
+        // Create a new game
+        BlackjackGUI game = new BlackjackGUI();
+
+        // Play the game
+        game.playGame();
+             
+    }
+}
