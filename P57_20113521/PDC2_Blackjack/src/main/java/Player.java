0f232347d3c5a9488d3d929c209f8f3b7ca93647
Tobyko48 <tobyko48@gmail.com>
2023-10-27 20:49:43 +1300

    Updated Player class
    improve readability, remove unnecessary variables and methods

--- P57_20113521/PDC2_Blackjack/src/main/java/Player.java
+++ P57_20113521/PDC2_Blackjack/src/main/java/Player.java
@@ -9,85 +9,46 @@
  */
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class Player implements PlayerInterface {
-    private String name;
-    private List<Card> hand;
-    private String password; // Add a password field
-    private int wins = 0;
-    private int losses = 0;
+    private ArrayList<Card> hand;
+    private Deck deck;
 
-    public Player(String name) {
-        this.name = name;
-        this.hand = new ArrayList<>();
+    public Player(Deck deck) {
+        this.deck = deck;
+        this.hand = new ArrayList<Card>();
+        this.hand.add(deck.draw());
+        this.hand.add(deck.draw());
     }
 
     @Override
-    public void addCard(Card card) {
-        hand.add(card);
-    }
-
-    @Override
-    public List<Card> getHand() {
+    public ArrayList<Card> getHand() {
         return hand;
     }
-
+    
     @Override
-    public String getName() {
-        return name;
+    public void hit(Deck deck) {
+        hand.add(deck.draw());
     }
-
+    
     @Override
-    public int getHandValue() {
-        int value = 0;
-        int numAces = 0;
-
+    public int sum() {
+        int totalValue = 0;
+        int aces = 0;
         for (Card card : hand) {
             if (card.getRank().equals("Ace")) {
-                numAces++;
-                value += 11;
-            } else if (card.getRank().equals("King") || card.getRank().equals("Queen") || card.getRank().equals("Jack")) {
-                value += 10;
-            } else {
-                value += Integer.parseInt(card.getRank());
+                aces++;
             }
+            totalValue += card.getValue();
         }
-
-        while (value > 21 && numAces > 0) {
-            value -= 10;
-            numAces--;
+        while (totalValue > 21 && aces > 0) {
+            totalValue -= 10;
+            aces--;
         }
-
-        return value;
+        return totalValue;
     }
-    
-    public void setPassword(String password) {
-        this.password = password;
-    }
-    
-    public String getPassword() {
-        return password;
-    }
-    
-    public int getWins() {
-    return wins;
-}
 
-public void setWins(int wins) {
-    this.wins = wins;
-}
-
-public int getLosses() {
-    return losses;
-}
-
-public void setLosses(int losses) {
-    this.losses = losses;
-}
-
-  @Override
-    public void clearHand() {
-        hand.clear();
+    public boolean hasBusted() {
+        return sum() > 21;
     }
 }
