81e147bd360a0560d5d9497c0e58e7279a5cb626
Tobyko48 <tobyko48@gmail.com>
2023-10-27 20:46:20 +1300

    Update Deck.java
    Improved readability, and methods

--- P57_20113521/PDC2_Blackjack/src/main/java/Deck.java
+++ P57_20113521/PDC2_Blackjack/src/main/java/Deck.java
@@ -10,41 +10,22 @@
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.List;
-
 
 public class Deck {
     
-    private List<Card> cards;
+    private ArrayList<Card> cards;
 
     public Deck() {
-        initializeDeck();
-        shuffle();
-    }
-
-    private void initializeDeck() {
         cards = new ArrayList<>();
-
-        String[] suits = {"Hearts", "Diamonds", "Clubs", "Spades"};
-        String[] ranks = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"};
-
-        for (String suit : suits) {
-            for (String rank : ranks) {
-                cards.add(new Card(suit, rank));
+        for (String suit : new String[]{"Hearts", "Diamonds", "Clubs", "Spades"}) {
+            for (String rank : new String[]{"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"}) {
+                cards.add(new Card(rank, suit));
             }
         }
-    }
-
-    public void shuffle() {
         Collections.shuffle(cards);
     }
 
-    public Card dealCard() {
-        if (cards.isEmpty()) {
-            initializeDeck();
-            shuffle();
-        }
-
+    public Card draw() {
         return cards.remove(cards.size() - 1);
     }
 }
